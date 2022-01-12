9c462b502e5a4db48099dc4a2fd9240000b25bba
Lasha-Giorgi Intskirveli <giorgilasha3@gmail.com>
2022-01-12 16:25:33 +0400

    final commit

--- scripts/server.js
+++ scripts/server.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+const express = require("express");
+const path = require("path");
+const open = require("open");
 
-import webpack from 'webpack';
-import config from '../webpack.config';
+const webpack = require("webpack");
+const config = require("../webpack.config");
 
 const port = 3000;
 const app = express();
 
 const compiler = webpack(config);
 app.use(
-  require('webpack-dev-middleware')(compiler, {
+  require("webpack-dev-middleware")(compiler, {
     publicPath: config.output.publicPath,
   })
 );
 
-app.use(express.static('src'));
+app.use(express.static("src"));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    open("http://localhost:" + port);
   }
 });
